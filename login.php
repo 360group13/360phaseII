423e5578c8aeaef1bd83cd640c6b53407d3e4ded
Elliott <elliottedward327@gmail.com>
2013-04-01 16:35:39 -0700

    Progress!

--- login.php
+++ login.php
@@ -1,12 +1,34 @@
 <?php
 
-$con=mysql_connect("localhost", "username", "password");
-mysql_select_db("wellcheckclinic", $con);
+mysql_connect('localhost', 'root', '') or die(mysql_error());
+mysql_select_db('wellcheckclinic') or die(mysql_error());
 
 $newUser = $_POST["user"];
+$newPass = $_POST["password"];
 
-$query = "SELECT userType FROM accounts where username=$newUser";
+$result = mysql_query("SELECT * FROM accounts WHERE username = '$newUser' AND password = '$newPass'");
 
-header("refresh: 0; http://localhost/360phaseII/patientwelcome.html");
+$type = -1;
 
-?>
+while($row = mysql_fetch_array($result))
+{
+    $type = $row['userType'];
+    if($type == 0)
+    { 
+        header("refresh: 0; http://localhost/360phaseII/patientwelcome.html");
+    }
+    if($type == 1)
+    {
+        header("refresh: 0; http://localhost/360phaseII/login.html");
+    }
+    if($type == 2)
+    {
+        header("refresh: 0; http://localhost/360phaseII/login.html");
+    }
+    if($type == -1)
+    {
+        echo "Invalid username and/or password.";
+    }
+}
+
+?> 
