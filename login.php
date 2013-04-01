5cc69b446d6a1b4bc4b08320a45d3afb2c8ea618
Elliott <elliottedward327@gmail.com>
2013-04-01 14:45:23 -0700

    Upload!

--- login.php
+++ login.php
@@ -1,5 +1,12 @@
 <?php
 
-echo $_POST["user"];
+$con=mysql_connect("localhost", "username", "password");
+mysql_select_db("wellcheckclinic", $con);
+
+$newUser = $_POST["user"];
+
+$query = "SELECT userType FROM accounts where username=$newUser";
+
+header("refresh: 0; http://localhost/360phaseII/patientwelcome.html");
 
 ?>
