1633063a077276c5f1141502eeeb0765ac17b467
Elliott <elliottedward327@gmail.com>
2013-04-04 14:07:16 -0700

    HELLO

--- doctorcomment.php
+++ doctorcomment.php
@@ -5,7 +5,10 @@ mysql_select_db('wellcheckclinic') or die(mysql_error());
 
 $newComment = $_POST['comment'];
 
-mysql_query("INSERT INTO patient(comment) VALUE('$newComment);");
+mysql_query("INSERT INTO patient(comment) VALUE('$newComment)';");
+
+echo "Comment Successfully Added.";
+header("refresh: 3; url=http://localhost/360phaseII/doctorwelcome.html");
 
 ?>
 
--- doctorprescribe.php
+++ doctorprescribe.php
@@ -5,7 +5,10 @@ mysql_select_db('wellcheckclinic') or die(mysql_error());
 
 $newPrescription = $_POST['prescription'];
 
-mysql_query("INSERT INTO patient(prescriptions) VALUE('$newPrescription);");
+mysql_query("INSERT INTO patient(prescriptions) VALUE('$newPrescription)';");
+
+echo "Prescription Successfully Added.";
+header("refresh: 3; url=http://localhost/360phaseII/doctorwelcome.html");
 
 ?>
 
